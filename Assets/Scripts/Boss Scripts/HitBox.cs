18f00941b1af179552be009f90635b1227d84eb0
Lykee <shem0710@gmail.com>
2017-06-05 01:57:52 +0800

    -Added singleton-ed player class

--- Assets/Scripts/Boss Scripts/HitBox.cs
+++ Assets/Scripts/Boss Scripts/HitBox.cs
@@ -5,19 +5,30 @@ public class HitBox : MonoBehaviour
 {
     private int m_Health;
     public int m_MoveSpeed;
+    public float m_AttackDelayTime;
+    private Player player; 
+    float m_AttackTimer;
+    Vector3 m_TargetLocation;
 
+    enum AI_STATE
+    {
 
+    };
 
 	// Use this for initialization
 	void Start ()
     {
+        GetComponent<Renderer>().material.color = new Color(1, 0, 0);
         m_Health = 5;
         m_MoveSpeed = 10;
+        m_AttackDelayTime = 3;//3 s between
+        player = GetComponent<Player>();
         
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
