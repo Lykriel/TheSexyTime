744729492ee9fe7652a106204eb58584e3cac34d
sheepborn <SheepBorn98@gmail.com>
2017-06-05 00:58:12 +0800

    - Added Range for attacks

---
+++ Assets/Scripts/Player Scripts/Actions.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Actions : MonoBehaviour {
+
+
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindTargets();
+        }
+    }
+
+    void FindTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetInRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetInRadius.Length; i++)
+        {
+            Transform target = targetInRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle (transform.forward,dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget,obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+            
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegree, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegree += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegree * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegree * Mathf.Deg2Rad));
+    }
+
+}
---
+++ Assets/Scripts/Player Scripts/AttackRangeEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (Actions))]
+
+public class AttackRangeEditor : Editor {
+
+    private void OnSceneGUI()
+    {
+        Actions fow = (Actions)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTargets in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTargets.position);
+        }
+    }
+}
