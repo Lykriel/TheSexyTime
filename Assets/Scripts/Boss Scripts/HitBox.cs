15137ee66a0266ae70bec5a26cdacd46c41482df
Lykee <shem0710@gmail.com>
2017-06-05 00:54:47 +0800

    -Added singleton parent class -Added Extra level to build boss (empty level)

--- Assets/Scripts/Boss Scripts/HitBox.cs
+++ Assets/Scripts/Boss Scripts/HitBox.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class HitBox : MonoBehaviour
 {
-    int m_Health;
+    private int m_Health;
+    public int m_MoveSpeed;
+
 
 
 	// Use this for initialization
 	void Start ()
     {
         m_Health = 5;
+        m_MoveSpeed = 10;
+        
 	}
 	
 	// Update is called once per frame
