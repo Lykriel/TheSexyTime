832d51a281ebde76a82cfeff345c16681bf405ac
Lykee <shem0710@gmail.com>
2017-07-03 04:48:44 +0800

    Added new boss prefab(VERY BUGGY PROTOTYPE)

---
+++ Assets/Scripts/Boss Scripts/Basic_Orbital_1.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basic_Orbital_1 : MonoBehaviour
+{
+
+    Vector3 ParentObjPos;
+    TheFirstGuardian CenterPiece;
+    
+    enum OrbitalState
+    {
+        s_Protect,
+        s_Extend,
+        s_Retract,
+    };
+
+    OrbitalState m_State;
+    Vector3 m_OrgPos;
+    Vector3 m_Dir;
+    float m_Timer;
+	// Use this for initialization
+	void Start ()
+    {
+        CenterPiece = GetComponentInParent<TheFirstGuardian>();
+        m_State = OrbitalState.s_Extend;
+        m_OrgPos = transform.position;
+        m_Timer = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ParentObjPos = transform.parent.position;
+
+        if(m_State == OrbitalState.s_Extend)
+        {
+            m_Dir = new Vector3(transform.position.x - ParentObjPos.x, 0, transform.position.z - ParentObjPos.z).normalized;
+            transform.Translate(m_Dir * Time.deltaTime);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/Boss Scripts/TheFirstGuardian.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheFirstGuardian : MonoBehaviour {
+
+    protected enum AI_STATE
+    {
+
+        s_Aiming,
+        s_Patrol,
+        s_Rest,
+        s_Stunned,
+        s_TripleAttack
+
+    };
+    enum MORTALITY_STATE
+    {
+        SpankMeDaddy,
+        NutsackOfSteel,
+        FuckFarOff
+
+    };
+
+    private int m_Health;
+    public int m_MoveSpeed;
+    public GameObject player;
+    int m_AttackCount;
+    int m_SpinFactor;
+    protected AI_STATE m_State;
+    MORTALITY_STATE m_Mortality;
+
+    //state stuff
+    float m_StateTimer;
+    float m_StateLifeSpan;
+    Vector3 m_TargetLocation;
+    bool m_IsRandomed;
+    bool m_IsAim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Mortality = MORTALITY_STATE.NutsackOfSteel;
+        m_State = AI_STATE.s_Patrol;
+        m_Health = 1;
+        m_MoveSpeed = 10;
+        m_IsRandomed = false;
+        m_IsAim = false;
+        m_SpinFactor = 75;
+        m_AttackCount = 0;
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        m_Health -= damage;
+        if (m_Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //change color depending on mortality
+        if (m_Mortality == MORTALITY_STATE.FuckFarOff)
+        {
+            GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+            m_SpinFactor = 400;
+        }
+        else
+        {
+            m_SpinFactor = 75;
+        }
+
+        if (m_Mortality == MORTALITY_STATE.SpankMeDaddy)
+        {
+            GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+        }
+
+        if (m_Mortality == MORTALITY_STATE.NutsackOfSteel)
+        {
+            GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+        }
+
+        //state changing and AI
+        switch (m_State)
+        {
+            //patrol 3-8 seconds(immortal)
+            case AI_STATE.s_Patrol:
+                m_Mortality = MORTALITY_STATE.NutsackOfSteel;
+                if (!m_IsRandomed)
+                {
+                    m_StateLifeSpan = Random.Range(8, 12);
+                    m_IsRandomed = true;
+                }
+                if (!m_IsAim)
+                {
+                    m_TargetLocation = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+                    m_IsAim = true;
+                }
+                if (m_IsAim)
+                {
+                    Vector3 move = (m_TargetLocation - transform.position).normalized;
+                    move.y = 0;
+                    transform.Translate(move * Time.deltaTime * 5, Space.World);
+                    if (Vector3.Distance(m_TargetLocation, transform.position) <= 1.0f)
+                    {
+                        m_IsAim = false;
+                    }
+
+                }
+                m_StateTimer += Time.deltaTime;
+                if (m_StateTimer >= m_StateLifeSpan)
+                {
+                    m_State = AI_STATE.s_Aiming;
+                    m_StateTimer = 0.0f;
+                    m_IsAim = false;
+                    m_IsRandomed = false;
+                }
+
+
+                break;
+            //aim for 1 second and wait for 0.5 seconds(vulnerable)
+            case AI_STATE.s_Aiming:
+                if (!m_IsAim)
+                {
+                    m_Mortality = MORTALITY_STATE.SpankMeDaddy;
+                    m_TargetLocation = player.transform.position;
+                    m_StateTimer += Time.deltaTime;
+                    if (m_StateTimer >= 1.0f)
+                    {
+                        m_StateTimer = 0.0f;
+                        m_IsAim = true;
+                    }
+
+
+                }
+                if (m_IsAim)
+                {
+                    m_Mortality = MORTALITY_STATE.FuckFarOff;
+                    m_StateTimer += Time.deltaTime;
+                    if (m_StateTimer >= 0.5f)
+                    {
+                        m_StateTimer = 0.0f;
+                        m_IsAim = false;
+                        m_State = AI_STATE.s_TripleAttack;
+                        m_AttackCount += 1;
+                    }
+                }
+                break;
+
+            //dash forward (attack )
+            case AI_STATE.s_TripleAttack:
+                Vector3 moveVec = (m_TargetLocation - transform.position).normalized;
+                moveVec.y = 0;
+                transform.Translate(moveVec * Time.deltaTime * 20,Space.World);
+                if (Vector3.Distance(m_TargetLocation, transform.position) <= 1.0f)
+                {
+                    if (m_AttackCount < 4)
+                    {
+                        m_State = AI_STATE.s_Aiming;
+                    }
+                    else
+                    {
+                        m_AttackCount = 0;
+                        m_State = AI_STATE.s_Patrol;
+                    }
+
+                }
+                break;
+
+        }
+        transform.Rotate(transform.up * Time.deltaTime * m_SpinFactor, Space.World);
+    }
+
+}
